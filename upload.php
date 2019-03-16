f6dedf3d3e1f692cf70afbeb816770639e44cbdc
rushikc <1ms16cs021@gmail.com>
2019-03-16 19:08:51 +0530

    updated

--- upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-$txt = "Vinyas\n";
-fwrite($myfile, $txt);
-$txt = "Rushi\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-?>
